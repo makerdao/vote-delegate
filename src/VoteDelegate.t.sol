ab9669889ca738d7fc728ed9d49733b8050cf867
Kurt Barry <kurt.m.barry@gmail.com>
2021-05-28 14:14:06 -0400

    fix license headers

--- src/VoteDelegate.t.sol
+++ src/VoteDelegate.t.sol
@@ -2,7 +2,8 @@
 
 // Copyright (C) 2021 Dai Foundation
 
-// This program is free software: you can redistribute it and/or modify             // it under the terms of the GNU Affero General Public License as published by
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
